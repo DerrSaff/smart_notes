822a61088ecf40c39d91f0590dbf12ef3e5c56dc
Andrew Gladilin <andrewgladilin@gmail.com>
2017-06-08 17:32:42 +0300

    Add Create a Note form

--- app/src/main/java/com/test/smartnotes/CreateNoteActivity.java
+++ app/src/main/java/com/test/smartnotes/CreateNoteActivity.java
@@ -2,6 +2,8 @@ package com.test.smartnotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CreateNoteActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class CreateNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
+        Spinner importanceSpinner = (Spinner) findViewById(R.id.importanceSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.importanceItems, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        importanceSpinner.setAdapter(adapter);
     }
 }
